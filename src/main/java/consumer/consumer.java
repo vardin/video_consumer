5ff4383a51991991e1795d47f688c863035466dc
vardin <vardin@starcom>
2016-12-03 22:05:02 +0900

    init source

--- src/main/java/consumer/consumer.java
+++ src/main/java/consumer/consumer.java
@@ -15,12 +15,15 @@ import kafka.message.MessageAndMetadata;
 
 
 public class consumer{
-    private static final String TOPIC = "test";
+    private static final String TOPIC = "supercom";
     private static final int NUM_THREADS = 20;
+   
+       
     public static void main(String[] args) throws Exception {
+    	
         Properties props = new Properties();
         props.put("group.id", "test-group");
-        props.put("zookeeper.connect", "kafka1:2181,kafka2:2181,kafka3:2181");
+        props.put("zookeeper.connect", "163.152.47.94:2182");
         props.put("auto.commit.interval.ms", "100");
         ConsumerConfig consumerConfig = new ConsumerConfig(props);
         ConsumerConnector consumer = Consumer.createJavaConsumerConnector(consumerConfig);
@@ -29,23 +32,42 @@ public class consumer{
         Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
         List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(TOPIC);
         ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+        long startTime = System.currentTimeMillis();
+        
         for (final KafkaStream<byte[], byte[]> stream : streams) {
-        	
-        	    
-            executor.execute(new Runnable() {
-            
+        	        	    
+           executor.execute(new Runnable() {
+          int count=0;
+          long startTime = System.currentTimeMillis();
                 public void run() {
                     for (MessageAndMetadata<byte[], byte[]> messageAndMetadata : stream) {
+                    	                  	
+                    	byte[] test;
                     	
-                    	String str="";
-                    	str = new String(messageAndMetadata.message());
-                    	str = str + "    Test";
-                        System.out.println(str);
+                    	test=messageAndMetadata.message();
+                    	count++;
+                    	System.out.println("one_complete!"+" "+count);
+               //     	String str ="";
+                                     	
+               //     	str = new String(messageAndMetadata.message());
+              //      	str = str + "    Test";
+                 //       System.out.println(str);
                     }
                 }
+     
+          
             });
+           
+         
         }
-        Thread.sleep(600);
+        long endTime = System.currentTimeMillis();
+        long Result_Time = endTime - startTime;
+        
+        System.out.println("Result : " + Result_Time + "(ms)");
+        
+        
+        Thread.sleep(60000);
+        System.out.println("empty topic!!");
         consumer.shutdown();
         executor.shutdown();
     }
